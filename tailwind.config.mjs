0350e5c7ee418bd01478ebab171be8a225ee418b
Mufaddal Aurangabadi <mufaddalaurangabadi@gmail.com>
2025-02-23 04:42:31 +0530

    final

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,10 +1,3 @@
-const defaultTheme = require("tailwindcss/defaultTheme");
- 
-const colors = require("tailwindcss/colors");
-const {
-  default: flattenColorPalette,
-} = require("tailwindcss/lib/util/flattenColorPalette");
-
 /** @type {import('tailwindcss').Config} */
 export default {
     darkMode: ["class"],
@@ -84,14 +77,3 @@ export default {
   },
   plugins: [require("tailwindcss-animate")],
 };
-
-function addVariablesForColors({ addBase, theme }) {
-	let allColors = flattenColorPalette(theme("colors"));
-	let newVars = Object.fromEntries(
-	  Object.entries(allColors).map(([key, val]) => [`--${key}`, val])
-	);
-   
-	addBase({
-	  ":root": newVars,
-	});
-  }
