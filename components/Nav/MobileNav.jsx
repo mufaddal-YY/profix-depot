eb75286ecb1518767a804afb3f0f3f82f34365bc
Mufaddal Aurangabadi <mufaddalaurangabadi@gmail.com>
2025-03-02 17:37:08 +0530

    mobile nav color  changes

--- components/Nav/MobileNav.jsx
+++ components/Nav/MobileNav.jsx
@@ -17,6 +17,8 @@ import { FaWhatsapp } from "react-icons/fa";
 import { MdCall } from "react-icons/md";
 import Link from "next/link";
 import { AlignJustify } from "lucide-react";
+import Image from "next/image";
+import Logo from "@/public/logo.png";
 
 const MobileNav = ({ productsData, contactData }) => {
   return (
@@ -26,18 +28,20 @@ const MobileNav = ({ productsData, contactData }) => {
       </SheetTrigger>
       <SheetContent className="z-[1001]">
         <SheetTitle></SheetTitle>
-        <div className="grid py-4 list-none ">
+        <div className="grid py-4 list-none space-y-2 ">
           <Link
             href={"/"}
-            className="text-md mx-2 font-semibold text-primary border border-[#fff] px-4 py-2 hover:border-[#3e2522] transition-colors duration-600 ease-in-out">
+            className="text-md font-semibold text-white bg-primary px-4 py-2 hover:border-[#3e2522] transition-colors duration-600 ease-in-out">
             <SheetClose>Home</SheetClose>
           </Link>
           {productsData.map((items, idx) => (
             <DropdownMenu key={idx}>
               <DropdownMenuTrigger asChild>
-                <Link href={`/products/${items.slug}`}>
+                <Link
+                  className="text-white bg-primary"
+                  href={`/products/${items.slug}`}>
                   <SheetClose>
-                    <span className="text-md mx-2 cursor-pointer flex flex-row items-center gap-2 font-semibold text-primary border border-[#fff] px-4 py-2 hover:border-[#3e2522] transition-colors duration-600 ease-in-out">
+                    <span className="text-md mx-2 cursor-pointer flex flex-row items-center gap-2 font-semibold px-4 py-2 hover:border-[#3e2522] transition-colors duration-600 ease-in-out">
                       {items.headline} <IoIosArrowDown />
                     </span>
                   </SheetClose>
@@ -62,12 +66,12 @@ const MobileNav = ({ productsData, contactData }) => {
 
           <Link
             href={"/gallery"}
-            className="text-md mx-2 font-semibold text-primary border border-[#fff] px-4 py-2 hover:border-[#3e2522] transition-colors duration-600 ease-in-out">
+            className="text-md font-semibold text-white bg-primary px-4 py-2 hover:border-[#3e2522] transition-colors duration-600 ease-in-out">
             <SheetClose>Gallery</SheetClose>
           </Link>
           <Link
             href={"/about"}
-            className="text-md mx-2 font-semibold text-primary border border-[#fff] px-4 py-2 hover:border-[#3e2522] transition-colors duration-600 ease-in-out">
+            className="text-md font-semibold text-white bg-primary px-4 py-2 hover:border-[#3e2522] transition-colors duration-600 ease-in-out">
             <SheetClose>About</SheetClose>
           </Link>
         </div>
@@ -89,6 +93,21 @@ const MobileNav = ({ productsData, contactData }) => {
                 <FaWhatsapp />
               </span>
               Whatsapp us
+            </Link>
+            <Link
+              href={`https://api.whatsapp.com/send/?phone=%2B${items.whatsapp}&text=Hi&type=phone_number&app_absent=0`}
+              target="_blank"
+              className="-mx-3 flex items-center gap-2 rounded-lg px-3 text-md font-medium leading-7 text-gray-900 hover:bg-gray-50">
+              <Image src={Logo} width={250} height={150} alt="Logo" />
+            </Link>
+            <Link
+              href={`https://api.whatsapp.com/send/?phone=%2B${items.whatsapp}&text=Hi&type=phone_number&app_absent=0`}
+              target="_blank"
+              className="-mx-3 flex items-center gap-2 rounded-lg px-3 py-4 text-md font-medium leading-7 text-gray-900 hover:bg-gray-50">
+              <span className="text-lg text-[#3e2522]">
+                {/* <FaWhatsapp /> */}
+              </span>
+              Address: {items?.address}
             </Link>
           </div>
         ))}
