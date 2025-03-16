23ff49eeb1d1d1ea5bc981b89fe3bc15ca5e3823
Mufaddal Aurangabadi <mufaddalaurangabadi@gmail.com>
2025-03-16 23:07:18 +0530

    z index

--- components/WhatsAppButton.jsx
+++ components/WhatsAppButton.jsx
@@ -12,7 +12,7 @@ const WhatsAppButton = ({ contactData }) => {
             href={"https://wa.link/867bth"}
             className="fixed bottom-4 cursor-pointer right-4 w-20 h-20
             rounded-full bg-[#0bd455] text-white flex items-center
-            justify-center text-3xl font-bold shadow-lg">
+            justify-center text-3xl font-bold shadow-lg z-[1021]">
             <FaWhatsapp />
           </Link>
         </div>
