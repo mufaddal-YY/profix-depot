eb7dd8df9fa36196f0944310c194fb461173136a
Mufaddal Aurangabadi <mufaddalaurangabadi@gmail.com>
2025-07-02 15:21:09 +0530

    gtag update

--- app/(site)/layout.jsx
+++ app/(site)/layout.jsx
@@ -28,30 +28,26 @@ export default async function RootLayout({ children }) {
   const productsData = await getProductsData();
   return (
     <html lang="en">
-      <head>
-        {/* Google Tag Manager Script */}
-        <Script id="gtm-script" strategy="afterInteractive">
+      <body className={`${inter.className} antialiased`}>
+        {/* Google Ads */}
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=AW-17015153915"
+          strategy="afterInteractive"
+        />
+        <Script id="google-ads" strategy="afterInteractive">
           {`
-            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-            })(window,document,'script','dataLayer','GTM-PXB84SSJ');
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'AW-17015153915');
           `}
         </Script>
-      </head>
-      <body className={`${inter.className} antialiased`}>
-        {/* Google Tag Manager (noscript) fallback */}
-        <noscript>
-          <iframe
-            src="https://www.googletagmanager.com/ns.html?id=GTM-PXB84SSJ"
-            height="0"
-            width="0"
-            style={{ display: "none", visibility: "hidden" }}></iframe>
-        </noscript>
         <TopBar contactData={contactData} />
         <Header productsData={productsData} contactData={contactData} />
-        <main className="mt-32">{children}<Toaster closeButton position="top-right" richColors /></main>
+        <main className="mt-32">
+          {children}
+          <Toaster closeButton position="top-right" richColors />
+        </main>
         <Footer contactData={contactData} productsData={productsData} />
         <WhatsAppButton contactData={contactData} />
         <Suspense>
